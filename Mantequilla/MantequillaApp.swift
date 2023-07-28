0c849b2b2afd34dff765d8295dca7e605438b374
Yaroslav G <yarmcfly@gmail.com>
2023-07-28 13:20:48 +0300

    Initial Commit

---
+++ Mantequilla/MantequillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MantequillaApp.swift
+//  Mantequilla
+//
+//  Created by Yaroslav G on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MantequillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
